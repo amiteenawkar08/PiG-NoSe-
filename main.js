05e27c14e5a25eba4651a34abc271b8c080478f9
amiteenawkar08 <93813741+amiteenawkar08@users.noreply.github.com>
2022-06-14 13:05:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
+noseX=0;
+noseY=0;
 function preload(){
-
+    pig_nose = loadImage('https://i.postimg.cc/Jng188MZ/pig-nose.png');
 }
 function setup(){
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose' , gotPoses);
 }
-function draw(){    
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
-    function take_snapshot(){
-        save('LoL Image')
-    }
-    
+}
+
+function draw(){
+image(video, 0, 0, 300, 300);
+image(pig_nose,noseX,noseY,30,30);
+
+
+}
+function take_snapshot(){
+    save('LoL Image')
+}
+
+
